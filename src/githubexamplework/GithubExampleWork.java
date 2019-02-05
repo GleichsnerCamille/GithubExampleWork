b6f96d09133b70e32885c9d09e23aef4c9105684
cag23@students.ptcollege.edu <>
2019-02-04 20:52:54 -0600

    This is the first commit

---
+++ src/githubexamplework/GithubExampleWork.java
@@ -0,0 +1,16 @@
+
+package githubexamplework;
+
+
+public class GithubExampleWork {
+
+   
+    public static void main(String[] args) {
+        Gleichsner();
+    }
+    
+    public static void Gleichsner() {
+        System.out.println("Cami Gleichsner");
+    }
+    
+}
